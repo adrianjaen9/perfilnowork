a043d85126deda2aabf9ab82af00716ac00e2575
inlab <adrian.jaen@est.fib.upc.edu>
2017-07-06 19:01:10 +0200

    començant memory

--- app/src/main/java/com/navarro/albert/baseactivynav/activities/Activity2.java
+++ app/src/main/java/com/navarro/albert/baseactivynav/activities/Activity2.java
@@ -3,23 +3,202 @@ package com.navarro.albert.baseactivynav.activities;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.material.joanbarroso.flipper.CoolImageFlipper;
 import com.navarro.albert.baseactivynav.BaseActivity;
 import com.navarro.albert.baseactivynav.R;
 
-public class Activity2 extends BaseActivity {
-    Drawable one, two, three, four, one1, two1, three1, four1;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
-    @Override
+public class Activity2 extends BaseActivity implements View.OnClickListener {
+    Drawable p0, p1, p2, p3, p4, p5, p6, p7;
+    ImageView image1, image2, image3, image4, image5, image6, image7, image8,
+    image9, image10, image11, image12, image13, image14, image15, image16;
+    List< ImageView > fotos = new LinkedList<>();
+    List< Drawable> drawables = new LinkedList<>();
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
         setTitle("Activity2");
         setItemChecked();
+        image1 = (ImageView) findViewById(R.id.p1);
+        image2 = (ImageView) findViewById(R.id.p2);
+        image3 = (ImageView) findViewById(R.id.p3);
+        image4 = (ImageView) findViewById(R.id.p4);
+        image5 = (ImageView) findViewById(R.id.p5);
+        image6 = (ImageView) findViewById(R.id.p6);
+        image7 = (ImageView) findViewById(R.id.p7);
+        image8 = (ImageView) findViewById(R.id.p8);
+        image9 = (ImageView) findViewById(R.id.p9);
+        image10 = (ImageView) findViewById(R.id.p10);
+        image11 = (ImageView) findViewById(R.id.p11);
+        image12 = (ImageView) findViewById(R.id.p12);
+        image13 = (ImageView) findViewById(R.id.p13);
+        image14 = (ImageView) findViewById(R.id.p14);
+        image15 = (ImageView) findViewById(R.id.p15);
+        image16 = (ImageView) findViewById(R.id.p16);
+        fotos.add(image1);
+            fotos.add(image2);
+            fotos.add(image3);
+            fotos.add(image4);
+            fotos.add(image5);
+            fotos.add(image6);
+            fotos.add(image7);
+            fotos.add(image8);
+            fotos.add(image9);
+            fotos.add(image10);
+            fotos.add(image11);
+            fotos.add(image12);
+            fotos.add(image13);
+            fotos.add(image14);
+            fotos.add(image15);
+            fotos.add(image16);
+        Collections.shuffle(fotos);
+            drawables.add(p0);
+            drawables.add(p1);
+            drawables.add(p2);
+            drawables.add(p3);
+            drawables.add(p4);
+            drawables.add(p5);
+            drawables.add(p6);
+            drawables.add(p7);
+
     }
 
     @Override
     protected int whatIsMyId() {
         return R.id.activity2;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.p1:
+                int n = troba(fotos, image1);
+                if (n > 7) n = n - 8;
+                Drawable a = drawables.get(n);
+                CoolImageFlipper flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image1);
+                break;
+            case R.id.p2:
+                n = troba(fotos, image2);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image2);
+                break;
+            case R.id.p3:
+                n = troba(fotos, image3);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image3);
+                break;
+            case R.id.p4:
+                n = troba(fotos, image4);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image4);
+                break;
+            case R.id.p5:
+                n = troba(fotos, image5);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image5);
+                break;
+            case R.id.p6:
+                n = troba(fotos, image6);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image6);
+                break;
+            case R.id.p7:
+                n = troba(fotos, image7);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image7);
+                break;
+            case R.id.p8:
+                n = troba(fotos, image8);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image8);
+                break;
+            case R.id.p9:
+                n = troba(fotos, image9);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image9);
+                break;
+            case R.id.p10:
+                n = troba(fotos, image10);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image10);
+                break;
+            case R.id.p11:
+                n = troba(fotos, image11);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image11);
+                break;
+            case R.id.p12:
+                n = troba(fotos, image12);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image12);
+                break;
+            case R.id.p13:
+                n = troba(fotos, image13);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image13);
+                break;
+            case R.id.p14:
+                n = troba(fotos, image14);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image14);
+                break;
+            case R.id.p15:
+                n = troba(fotos, image15);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image15);
+                break;
+            case R.id.p16:
+                n = troba(fotos, image16);
+                if (n > 7) n = n - 8;
+                a = drawables.get(n);
+                flipper = new CoolImageFlipper(this);
+                flipper.flipImage(a, image16);
+                break;
+        }
+    }
+    public int troba (List<ImageView> a, ImageView b){
+        for (int i = 0; i < 16; ++i){
+            if (b == a.get(i)) return i;
+        }
+        return 0;
     }
 }
